7e0844ab361d0010d11ad925e05317e42f957cc0
sultanparvez <sultanparvez77.sp@gmail.com>
2022-08-13 23:48:13 +0600

    clean up

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,4 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
+
 Cypress.Commands.add('selectProduct', (ProductName) => {  
 cy.get('h4.card-title').each(($el,index,$list)=>{
     if( $el.text().includes(ProductName)){
@@ -19,14 +6,3 @@ cy.get('h4.card-title').each(($el,index,$list)=>{
     }
  })
 })
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
