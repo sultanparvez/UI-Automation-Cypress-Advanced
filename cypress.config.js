a1041171fb58e0c2ba1fbb6a4b4d7e6bb9ed98e1
sultanparvez <sultanparvez77.sp@gmail.com>
2022-08-14 03:08:42 +0600

    added reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'mj5izw',
+  reporter:"cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Automation Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts:false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
